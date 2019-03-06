8c153b9af166887790a745b6dad7f524d30d0965
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-03-06 15:19:33 +0000

    Overhauled function.  Instead of generating a text report it generates an XML response containing ALL Snapshots for ALL VMs and uses Invoke-WebRequest to submit this to PRTG

--- Get-SnapshotReport.ps1
+++ Get-SnapshotReport.ps1
@@ -2,26 +2,33 @@ Function Get-SnapshotReport {
     Param (
         [String]$ESXiServer,
         [String]$ESXiUsername,
-        [SecureString]$ESXiSecurePassword
+        [SecureString]$ESXiSecurePassword,
+        [Uri]$PrtgUri = "http://brazil.bridgepartners.local:5050/9F53BAD6-E2FF-40B1-A25A-C845FF8CA182"
     )
     Write-Debug -Message "ESXiSecurePassword = $ESXiSecurePassword"
     $ESXiCredentials = New-Object System.Management.Automation.PSCredential($ESXiUsername,$ESXiSecurePassword)
     Connect-VIServer -Server $ESXiServer -Credential $ESXiCredentials
 
+    [XML]$xmlBody = New-Object System.Xml.XmlDocument
+    $xmlDeclaration = $xmlBody.CreateXmlDeclaration("1.0","UTF-8",$null)
+    $xmlBody.AppendChild($xmlDeclaration) | Out-Null
+    $xmlRoot = $xmlBody.CreateNode("element","prtg",$null)
+
     Get-VM | Foreach-Object {
         $vm = $_.Name
         Get-Snapshot -VM $_.Name | Foreach-Object {
-            $Report = "" | Select-Object Name,Description,State,"VM Name","Created Date","Size (MB)","Is Current","Age (Days)"
-            $Report."VM Name" = $vm
-            $Report.Name = $_.Name
-            $Report.Description = $_.Description
-            $Report.State = $_.PowerState
-            $Report."Created Date" = $_.Created
-            $Report."Size (MB)" = $_.SizeMB
-            $Report."Is Current" = $_.IsCurrent
-            $Report."Age (Days)" = ((Get-Date) - $_.Created).TotalDays
-            $Report
+            $xmlResult = $xmlBody.CreateNode("element","result",$null)
+            $xmlChannel = $xmlBody.CreateNode("element","channel",$null)
+            $xmlChannel.InnerText = $vm
+            $xmlValue = $xmlBody.CreateNode("element","value",$null)
+            $xmlValue.InnerText = ((Get-Date) - $_.Created).Days
+            $xmlResult.AppendChild($xmlChannel)
+            $xmlResult.AppendChild($xmlValue)
+            $xmlRoot.AppendChild($xmlResult)
         }
     }
-    Disconnect-VIServer -Server $ESXiServer
+    Disconnect-VIServer -Server $ESXiServer -Confirm:$false
+    $xmlBody.AppendChild($xmlRoot)
+    #Invoke-RestMethod -Method POST -ContentType "application/xml" -UseBasicParsing -Uri $PrtgUri -Body $xmlBody
+    $xmlBody.OuterXml
 }
